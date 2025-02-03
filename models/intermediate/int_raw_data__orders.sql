8897444fed2333152f33ad83cf997c8c659c5d3f
Clara Jouy <clara.jouy@orange.fr>
2025-02-03 15:46:33 +0000

    Adding the int_raw_data__orders to aggregated data at order level and have more informations at order level. Adding two tests for this model: - assert_total_order_amount_is_positive - assert_total_order_count_is_positive To control the data quality.
    Adding the markdown and yml files to document the project.

--- models/intermediate/int_raw_data__orders.sql
+++ models/intermediate/int_raw_data__orders.sql
@@ -1,14 +1,13 @@
 WITH order_item_informations AS (
     SELECT 
-        sum(quantity) as cnt_items_per_order,
+        order_id,
+        sum(unit_quantity) as cnt_items_per_order,
         count(distinct(product_id)) as cnt_diff_products_per_order,
         sum(total_order_item_amount) as total_order_amount,
-        sum(quantity*discount_amount_per_unit) as total_order_discount
+        sum(unit_quantity*discount_amount_per_unit) as total_order_discount
     FROM {{ref("stg_raw_data__order_items")}}
     GROUP BY order_id
 )
-
-
 
 SELECT 
     O.order_id,
@@ -18,7 +17,10 @@ SELECT
     total_order_discount,
     O.customer_id,
     C.customer_name,
+    C.customer_city,
+    C.customer_state,
     S.staff_name, 
+    S.manager_name,    
     So.store_name,
     O.order_date,
     O.order_status,
@@ -30,4 +32,6 @@ LEFT JOIN {{ref('stg_raw_data__customers')}} C
 ON O.customer_id = C.customer_id 
 LEFT JOIN {{ref('stg_raw_data__stores')}} So
 ON O.store_id = So.store_id
+LEFT JOIN order_item_informations OI
+ON O.order_id = OI.order_id
 
---
+++ tests/assert_total_order_count_is_positive.sql
@@ -0,0 +1,6 @@
+select
+    order_id,
+    sum(quantity) as cnt_items_per_order
+from {{ ref('stg_raw_data__order_items') }}
+group by order_id
+having cnt_items_per_order <= 0
